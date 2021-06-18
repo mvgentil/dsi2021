9c1f0ad84c42501b93b3d01b15d139ca644e6d8f
matheus-gentil <matheusvgentil@hotmail.com>
2021-06-18 01:22:36 +0000

    listagem de clientes

--- src/main/java/br/univille/mvgentildsi2021/controller/ClienteController.java
+++ src/main/java/br/univille/mvgentildsi2021/controller/ClienteController.java
@@ -1,17 +1,28 @@
 package br.univille.mvgentildsi2021.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.mvgentildsi2021.model.Cliente;
+import br.univille.mvgentildsi2021.service.ClienteService;
+
+
 @Controller
 @RequestMapping("/cliente")
 public class ClienteController {
+
+  @Autowired
+  private ClienteService service;
   
   @GetMapping
   public ModelAndView index(){
-    return new ModelAndView("cliente/index");
+    List<Cliente> listaClientes = service.getAll();
+    return new ModelAndView("cliente/index", "listaClientes", listaClientes);
   }
 
 }
--- src/main/java/br/univille/mvgentildsi2021/model/Cliente.java
+++ src/main/java/br/univille/mvgentildsi2021/model/Cliente.java
@@ -1,10 +1,13 @@
 package br.univille.mvgentildsi2021.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+
 
 @Entity
 public class Cliente extends Pessoa {
   
+  @Column(columnDefinition = "integer default 0")
   private int score;
 
   public int getScore() {
