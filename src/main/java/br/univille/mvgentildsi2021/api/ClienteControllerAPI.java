57abb02b49fe791143c79f028e411fd18fa705f7
matheus-gentil <matheusvgentil@hotmail.com>
2021-10-05 01:15:35 +0000

    finalizando APIde clientes

--- src/main/java/br/univille/mvgentildsi2021/api/ClienteControllerAPI.java
+++ src/main/java/br/univille/mvgentildsi2021/api/ClienteControllerAPI.java
@@ -7,9 +7,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,11 +43,23 @@ public class ClienteControllerAPI {
   }
 
   @GetMapping("/{id}")
-
   public ResponseEntity<Cliente> getById(@PathVariable("id") Cliente cliente){
-
       return new ResponseEntity<Cliente>(cliente,HttpStatus.OK);
-
   }
 
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Cliente> delete(@PathVariable("id") Cliente cliente){
+    service.delete(cliente);
+    return new ResponseEntity<>(cliente,HttpStatus.OK);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Cliente> update(@RequestBody Cliente clienteNovo, @PathVariable("id") Cliente clienteAntigo){
+    clienteAntigo.setCPF(clienteNovo.getCPF());
+    clienteAntigo.setNome(clienteNovo.getNome());
+    clienteAntigo.setTelefone(clienteNovo.getTelefone());
+    service.save(clienteAntigo);
+
+    return new ResponseEntity<>(clienteAntigo, HttpStatus.OK);
+  }
 }
