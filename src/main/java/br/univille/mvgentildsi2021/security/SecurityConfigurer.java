d7e5ea8c74ca37f950520a401f5417340cb0767f
matheus-gentil <matheusvgentil@hotmail.com>
2021-10-22 00:38:20 +0000

    corrigindo segurança do banco de dados

--- src/main/java/br/univille/mvgentildsi2021/security/SecurityConfigurer.java
+++ src/main/java/br/univille/mvgentildsi2021/security/SecurityConfigurer.java
@@ -3,6 +3,7 @@ package br.univille.mvgentildsi2021.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -13,6 +14,17 @@ import br.univille.mvgentildsi2021.service.impl.MyUserDetailsServiceImpl;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
+
+  @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+        .authorizeRequests()
+        .antMatchers("/fonte_dados/**").permitAll()
+        .anyRequest().authenticated().and().formLogin();
+ 
+        httpSecurity.csrf().ignoringAntMatchers("/fonte_dados/**");
+        httpSecurity.headers().frameOptions().sameOrigin();
+    }
   
   @Autowired
   private MyUserDetailsServiceImpl myUserDetailService;
